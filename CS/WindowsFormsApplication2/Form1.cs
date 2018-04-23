43654f269d33454966fdd769ddb682cac8bf5e6f
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,75 @@
+﻿using DevExpress.Utils;
+using DevExpress.XtraCharts;
+using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2 {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            // Create a new Chart control and add it to the form.
+            ChartControl chart = new ChartControl();
+            chart.Dock = DockStyle.Fill;
+            this.Controls.AddRange(new Control[] { chart });
+
+            // Create a Bar series and add points to it.
+            Series series1 = new Series("Bar", ViewType.Bar);
+            series1.Points.Add(new SeriesPoint("A", new double[] { 10 }));
+            series1.Points.Add(new SeriesPoint("B", new double[] { 32 }));
+            series1.Points.Add(new SeriesPoint("C", new double[] { 44 }));
+            series1.Points.Add(new SeriesPoint("D", new double[] { 67 }));
+
+            // Create a Line series and add points to it.
+            Series series2 = new Series("Line", ViewType.Line);
+            series2.Points.Add(new SeriesPoint("A", new double[] { 15 }));
+            series2.Points.Add(new SeriesPoint("B", new double[] { 18 }));
+            series2.Points.Add(new SeriesPoint("C", new double[] { 21 }));
+            series2.Points.Add(new SeriesPoint("D", new double[] { 27 }));
+
+            // Add the series to the chart.
+            chart.Series.Add(series1);
+            chart.Series.Add(series2);
+
+            // Cast the chart's diagram to the XYDiagram type, to access its axes.
+            XYDiagram diagram = (XYDiagram)chart.Diagram;
+
+            // Add a new additional pane to the diagram.
+            diagram.Panes.Add(new XYDiagramPane("My Pane"));
+
+            // Assign the additional pane  to the second series. 
+            // Note that the created pane has the zero index in the collection,
+            // because the existing Default pane is a separate entity.
+            LineSeriesView myView = (LineSeriesView)series2.View;
+            myView.Pane = diagram.Panes[0];
+
+            // Define the whole range for the axes.
+            diagram.AxisX.WholeRange.Auto = false;
+            diagram.AxisX.WholeRange.SetMinMaxValues("A", "D");
+            diagram.AxisY.WholeRange.Auto = false;
+            diagram.AxisY.WholeRange.SetMinMaxValues(0, 70);
+
+            // Define the visible range for the axes.  
+            diagram.AxisX.VisualRange.Auto = false;
+            diagram.AxisX.VisualRange.SetMinMaxValues("B", "C");
+            diagram.AxisY.VisualRange.Auto = false;
+            diagram.AxisY.VisualRange.SetMinMaxValues(7, 50);
+
+            // Specify the axes scrolling at the diagram's level.
+            diagram.EnableAxisXScrolling = true;
+            diagram.EnableAxisYScrolling = false;
+
+            // Individually specify the axes scrolling for the panes.
+            diagram.DefaultPane.EnableAxisXScrolling = DefaultBoolean.Default;
+            diagram.Panes[0].EnableAxisXScrolling = DefaultBoolean.False;
+            diagram.Panes[0].EnableAxisYScrolling = DefaultBoolean.True;
+
+            // Adjust how the scrolling can be performed.
+            diagram.ScrollingOptions.UseKeyboard = false;
+            diagram.ScrollingOptions.UseMouse = false;
+            diagram.ScrollingOptions.UseScrollBars = true;
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,9 @@
+# How to individually adjust scrolling along the axes
+
+
+<p>This example demonstrates how you can individually adjust scrolling along the X and Y axes. A chart with two <a href="http://larix/ReferenceBrowserMain_14_2/LoadItem.aspx?Member=D%3a5781&Template=CustomDocumentTopic">panes</a> is used to demonstrate this feature.<br /><br />In this example the chart with two series is created. The second series is assigned to an additional pane. <br />In addition, to show scrolling  bars along axes, the axis whole range and axis visible range is specified and the <a href="https://documentation.devexpress.com/#CoreLibraries/DevExpressXtraChartsScrollingOptions2D_UseScrollBarstopic">ScrollingOptions2D.UseScrollBars </a> property is set to<strong> true</strong>.  To learn more on axis ranges, refer to <a href="http://larix/ReferenceBrowserMain_14_2/LoadItem.aspx?Member=D%3a5803&Template=CustomDocumentTopic">Visual Ranges and Whole Ranges </a>.<br /><br />To specify scrolling along the axes of the diagram's panes, the  <a href="http://larix/ReferenceBrowserMain_14_2/LoadItem.aspx?Member=P%3aDevExpress.XtraCharts.XYDiagram2D.EnableAxisXScrolling&Template=MemberPropertyTopic">XYDiagram2D.EnableAxisXScrolling</a> and <a href="http://larix/ReferenceBrowserMain_14_2/LoadItem.aspx?Member=P%3aDevExpress.XtraCharts.XYDiagram2D.EnableAxisYScrolling&Template=MemberPropertyTopic">XYDiagram2D.EnableAxisYScrolling</a> properties are used.</p>
+<p>For more information, refer to <a href="http://larix/ReferenceBrowserMain_14_2/LoadItem.aspx?Member=D%3a2954&Template=CustomDocumentTopic">Zooming and Scrolling (2D XY-Charts)</a>.</p>
+
+<br/>
+
+
